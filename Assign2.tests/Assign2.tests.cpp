3e2bf2d3e2d5c418cb9b24cb40fb647589904fc6
Hunter Oswald <Hoswald1756@conestogac.on.ca>
2021-10-17 19:28:00 -0400

    Assign 2 - Task 2 tests written

--- Assign2.tests/Assign2.tests.cpp
+++ Assign2.tests/Assign2.tests.cpp
@@ -41,6 +41,90 @@ namespace Assign2tests
 			Assert::AreEqual(EXPECTED_AREA, actual);
 		}
 		
+		// set length and set width functions
+		TEST_METHOD(setLength_outsideOfRangeAbove_noChange)
+		{
+			// arrange
+			int rectLength = 3;
+			int setLen = 100;
+			int EXPECTED_LENGTH = 3;
+
+			// act
+			setLength(setLen, &rectLength);
+
+			// assert
+			Assert::AreEqual(EXPECTED_LENGTH, rectLength);
+		}
+
+		TEST_METHOD(setWidth_outOfRangeAbove_noChange)
+		{
+			// arrange
+			int rectWidth = 85;
+			int setWid = 100;
+			int EXPECTED_WID = 85;
+
+			// act
+			setWidth(setWid, &rectWidth);
+
+			// assert
+			Assert::AreEqual(EXPECTED_WID, rectWidth);
+		}
+
+		TEST_METHOD(setLength_outOfRangeBelow_noChange)
+		{
+			// arrange
+			int rectLength = 45;
+			int setLen = 0;
+			int EXPECTED_LEN = 45;
+
+			// act
+			setLength(setLen, &rectLength);
+
+			// assert
+			Assert::AreEqual(EXPECTED_LEN, rectLength);
+		}
+
+		TEST_METHOD(setWidth_outOfRangeBelow_noChange)
+		{
+			// arrange
+			int rectWidth = 34;
+			int setWid = 0;
+			int EXPECTED_WID = 34;
+
+			// act
+			setWidth(setWid, &rectWidth);
+
+			// assert
+			Assert::AreEqual(EXPECTED_WID, rectWidth);
+		}
+
+		TEST_METHOD(setLength_averageInput_changesRect)
+		{
+			// arrange
+			int rectLength = 4;
+			int setLen = 7;
+			int EXPECTED_LEN = 7;
+
+			// act
+			setLength(setLen, &rectLength);
+
+			// assert
+			Assert::AreEqual(EXPECTED_LEN, rectLength);
+		}
+
+		TEST_METHOD(setWidth_averageInput_changesRect)
+		{
+			// arrange
+			int rectWidth = 48;
+			int setWid = 99;
+			int EXPECTED_WID = 99;
+
+			// act
+			setLength(setWid, &rectWidth);
+
+			// assert
+			Assert::AreEqual(EXPECTED_WID, rectWidth);
+		}
 
 	};
 }
